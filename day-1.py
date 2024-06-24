4cf2cede231b8d2cb0660bc50cf6fa6a8eddd137
vaseemdane <153372053+vaseemdane@users.noreply.github.com>
2024-06-24 06:30:12 +0530

    Add files via upload

---
+++ day-1.py
@@ -0,0 +1,8 @@
+
+
+#  Day1_project
+# Band Name Generator
+print("Welcome to the Band Name Generator")
+a=(input("what's name of the city you grew up in?"))
+b=(input("what's the name of the pet?"))
+print("your band name could be=",a+" "+b)
